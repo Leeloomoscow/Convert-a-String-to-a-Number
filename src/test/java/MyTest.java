ce75fc8a892018d5402cae63f0c714ef098a6782
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-09-13 21:00:16 +0300

    Codewars Convert a String to a number

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,35 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void test1() {
+        assertEquals("stringToNumber(1234)", 1234, StringToNumber.stringToNumber());
+    }
 
+    @Test
+    public void test2() {
+        assertEquals("stringToNumber(605)", 605, StringToNumber.stringToNumber());
+    }
+
+    @Test
+    public void test3() {
+        assertEquals("stringToNumber(1405)", 1405, StringToNumber.stringToNumber());
+    }
+
+    @Test
+    public void test4() {
+        assertEquals("stringToNumber(-7)", -7, StringToNumber.stringToNumber());
+    }
+
+    private static class StringToNumber {
+        public static int stringToNumber(String str) {
+            int num = Integer.parseInt(str);
+            return num;
+        }
     }
 }
+
+
+
